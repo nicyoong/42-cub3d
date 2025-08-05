5eb6abe76f08f633c9485f8efb562eda51bcff67
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-08-05 21:01:50 +0800

    core : keypress movement incomplete w placeholder

---
+++ mandatory/src/keypress.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keypress.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tching <tching@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 20:56:31 by tching            #+#    #+#             */
+/*   Updated: 2025/08/05 21:00:31 by tching           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	keypress(int keycode, t_game *game)
+{
+	if (keycode == LEFT)
+		//
+	else if (keycode == RIGHT)
+		//
+	else if (keycode == 'a')
+		//
+	else if (keycode == 's')
+		//
+	else if (keycode == 'd')
+		//
+	else if (keycode == 'w')
+		//
+	else if (keycode == 'ESC')
+		exit_game(game);
+	return (0);
+}
