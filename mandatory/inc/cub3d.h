92e51ce68230c2ef8270857d8090a81ff27fd1e2
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-08-05 21:17:39 +0800

    add : more structs player, img, params, etc.

--- mandatory/inc/cub3d.h
+++ mandatory/inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tching <tching@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 20:42:03 by tching            #+#    #+#             */
-/*   Updated: 2025/08/05 20:50:33 by tching           ###   ########.fr       */
+/*   Updated: 2025/08/05 21:15:03 by tching           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,63 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <string.h>
+# include <../../libs/libft/libft.h>
 
 # define WINDOW_HEIGHT	720
 # define WINDOW_WIDTH	128
 
-typedef struct
+# define LEFT	65361
+# define RIGHT	65363
+# define UP	65362
+# define DOWN	65364
+# define ESC	65307
+
+enum	e_axis{
+	x,
+	y,
+	z
+};
+
+enum	e_direction{
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST
+};
+
+typedef struct s_image
+{
+	t_img		*img;
+	uint32_t	*buffer;
+}	t_image;
+
+typedef struct s_img_prop
+{
+	int		height;
+	int		width;
+	uint32_t	color;
+}	t_img_prop;
+
+typedef struct s_axis
+{
+	double		x;
+	double		y;
+}	t_axis;
+
+typedef struct s_player
+{
+	t_axis	xy;
+	int	player_direction;
+}	t_player;
+
+typedef struct s_params
+{
+	char	**texture;
+	char	**map;
+	char	**color[2];
+}	t_params;
+
+typedef struct s_game
 {
 	int		window_height;
 	int		window_width;
