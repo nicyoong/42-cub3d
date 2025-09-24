f6291504fa25e6f4aa0a542e10e1bff17997c50a
Nicholas Yoong <nicyoong321@gmail.com>
2025-09-24 20:26:04 +0800

    change asset

--- bonus/src/doors.c
+++ bonus/src/doors.c
@@ -13,7 +13,7 @@ static void load_door_anim(t_game *game, t_door *door)
     if ( set_sprite(&door->anim.frames[0], game->mlx, "./assets/sky_wall01.XPM") ||
          set_sprite(&door->anim.frames[1], game->mlx, "./assets/sky_wall01.XPM") ||
          set_sprite(&door->anim.frames[2], game->mlx, "./assets/sky_wall01.XPM") ||
-         set_sprite(&door->anim.frames[3], game->mlx, "./assets/sky_wall01.XPM") )
+         set_sprite(&door->anim.frames[3], game->mlx, "./assets/bark01.XPM") )
         error("Could not load door frames", game);
 }
 
