62711d1cb149ab9483541dfa39c5c63cde4a8654
Nicholas Yoong <nicyoong321@gmail.com>
2025-09-25 21:31:30 +0800

    partial norm cleanup_doors.c

--- bonus/src/cleanup_doors.c
+++ bonus/src/cleanup_doors.c
@@ -1,27 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup_doors.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 21:21:42 by nyoong            #+#    #+#             */
+/*   Updated: 2025/09/25 21:31:12 by nyoong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/cub3D.h"
 
-// Destroy one t_image safely (guard against NULLs)
-static void destroy_image_safe(void *mlx, t_image *img)
+static void	destroy_image_safe(void *mlx, t_image *img)
 {
-	if (img && img->img) {
+	if (img && img->img)
+	{
 		mlx_destroy_image(mlx, img->img);
 		img->img = NULL;
 		img->buffer = NULL;
 	}
 }
 
-// Free all door animation frames and the door array
-void cleanup_doors(t_game *game)
+void	cleanup_doors(t_game *game)
 {
-	if (!game) return;
-	if (!game->doors) return;
+	int		i;
+	t_door	*d;
+	int		f;
 
-	// Each door has door->anim.frames [0..frame_count-1]
-	for (int i = 0; i < game->door_count; i++) {
-		t_door *d = &game->doors[i];
-		if (d->anim.frames) {
-			for (int f = 0; f < d->anim.frame_count; f++) {
+	if (!game)
+		return;
+	if (!game->doors)
+		return;
+	i = 0;
+	while (i < game->door_count)
+	{
+		d = &game->doors[i];
+		if (d->anim.frames)
+		{
+			f = 0;
+			while (f < d->anim.frame_count)
+			{
 				destroy_image_safe(game->mlx, &d->anim.frames[f]);
+				f++;
 			}
 			free(d->anim.frames);
 			d->anim.frames = NULL;
@@ -29,18 +50,20 @@ void cleanup_doors(t_game *game)
 			d->anim.current = 0;
 			d->anim.timer = 0.0;
 		}
+		i++;
 	}
-
 	free(game->doors);
 	game->doors = NULL;
 	game->door_count = 0;
 }
 
 // Free zbuffer (depth buffer)
-void cleanup_zbuffer(t_game *game)
+void	cleanup_zbuffer(t_game *game)
 {
-	if (!game) return;
-	if (game->zbuffer) {
+	if (!game)
+		return;
+	if (game->zbuffer)
+	{
 		free(game->zbuffer);
 		game->zbuffer = NULL;
 	}
